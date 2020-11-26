b7c6f6ed57207fae537f769b6604d8cff4eb689b
Vivek Kadia <vivekjkadia@gmail.com>
2020-11-26 06:37:52 +0000

    final lab to call pages to category to mongodb

--- src/main/java/org/dell/kube/category/CategoryApplication.java
+++ src/main/java/org/dell/kube/category/CategoryApplication.java
@@ -2,7 +2,10 @@ package org.dell.kube.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CategoryApplication {
     public static void main(String[] args) {
